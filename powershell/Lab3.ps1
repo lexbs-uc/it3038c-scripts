1a1477c752c2da2df32b5cea357b300048c70cca
lexbs-uc <lexbs@mail.uc.edu>
2022-01-30 16:54:12 -0500

    powershell files

---
+++ powershell/Lab3.ps1
@@ -0,0 +1,12 @@
+﻿function getIP{
+(get-netipaddress).ipv4address | Select-String "192*"
+}
+write-host(getIP)
+$IP = getIP
+$Vers = (Get-Variable PSVersionTable -ValueOnly).PSVersion
+$Date = (Get-Date)
+
+
+$Body = ("This machine's IP is $IP. User is $env:UserName. Hostname is $env:ComputerName. Powershell version is $Vers. Today's date is $Date")
+
+Send-MailMessage -To "leonardf@ucmail.uc.edu" -From "daedlynwolf@gmail.com" -Subject "IT3038 Windows SysInfo" -Body $Body -SmtpServer smtp.gmail.com -port 587 -UseSSL -Credential (Get-Credential)
